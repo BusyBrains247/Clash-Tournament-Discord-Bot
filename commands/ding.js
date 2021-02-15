5c26f26d5d39079eb8af879f40ea28fd6e7e5994
CooperN <norman.cooper.j@gmail.com>
2021-02-15 16:44:20 -0600

    cleaning house

--- commands/ding.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ding',
-  description: 'sends dong!',
-  shortdescription: 'Sends dong!',
-  cooldown: 5,
-  execute(client, message, args, playerData){
-    message.channel.send("dong!").catch(console.error);
-  }
-};
--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'sends pong!',
-  shortdescription: 'Sends pong!',
-  cooldown: 5,
-  execute(client, message, args, playerData){
-    message.channel.send("pong!").catch(console.error);
-  }
-};
